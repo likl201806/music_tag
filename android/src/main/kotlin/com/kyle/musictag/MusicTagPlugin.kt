755a28cbd5083b824aa2b00e996db0a198d0b63c
李孔林 <weirongwei2016@126.com>
2024-05-21 14:27:07 +0800

    feat：修复android端bug

--- android/src/main/kotlin/com/kyle/musictag/MusicTagPlugin.kt
+++ android/src/main/kotlin/com/kyle/musictag/MusicTagPlugin.kt
@@ -1,27 +1,33 @@
-package com.example.music_tag
+package com.kyle.musictag
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
 
 class MusicTagPlugin : FlutterPlugin, MethodCallHandler {
     private var channel: MethodChannel? = null
-    
-    fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.getBinaryMessenger(), "music_tag")
-        channel.setMethodCallHandler(this)
+        channel!!.setMethodCallHandler(this)
     }
 
-    fun onMethodCall(call: MethodCall, result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method.equals("writeTag")) {
-            val filePath: String = call.argument("filePath")
-            val tags: Map<String, String> = call.argument("tags")
-            writeTag(filePath, tags)
+            val filePath: String? = call.argument("filePath")
+            val tags: Map<String, String>? = call.argument("tags")
+            if (filePath != null) {
+                if (tags != null) {
+                    writeTag(filePath, tags)
+                }
+            }
             result.success(null)
         } else if (call.method.equals("readTag")) {
-            val filePath: String = call.argument("filePath")
-            val tags = readTag(filePath)
+            val filePath: String? = call.argument("filePath")
+            val tags = filePath?.let { readTag(it) }
             result.success(tags)
         } else {
             result.notImplemented()
@@ -37,7 +43,7 @@ class MusicTagPlugin : FlutterPlugin, MethodCallHandler {
         return HashMap()
     }
 
-    fun onDetachedFromEngine(binding: FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel!!.setMethodCallHandler(null)
     }
 }
